f4f56a79595c7ff690740dedde5153997721c3ef
Brendon Ignath <brendon.ignath@gmail.com>
2025-02-20 19:19:53 +0100

    Redirect to first step

--- src/modules/products/components/product-actions/index.tsx
+++ src/modules/products/components/product-actions/index.tsx
@@ -117,10 +117,7 @@ export default function ProductActions({
     }
 
     setIsAdding(false);
-    // Added delay to ensure state updates before redirecting to checkout
-    setTimeout(() => {
-      router.push(`/${countryCode}/checkout`);
-    }, 300);
+    router.push(`/${countryCode}/checkout?step=address`);
   };
 
   return (
