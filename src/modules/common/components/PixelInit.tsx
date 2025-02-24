dc9a8b11d049325b7d6525d87c05c32494247bf3
Brendon Ignath <brendon.ignath@gmail.com>
2025-02-24 12:14:22 +0100

    Fix pixel

--- src/modules/common/components/PixelInit.tsx
+++ src/modules/common/components/PixelInit.tsx
@@ -5,19 +5,25 @@ export default function PixelInit() {
   useEffect(() => {
     if (typeof window === "undefined") return;
 
-    import("react-facebook-pixel").then((ReactPixel) => {
-      ReactPixel.init(process.env.PIXEL_ID ?? "");
-      ReactPixel.pageView();
-      console.log("Pixel initialized");
-    });
+    const script = document.createElement("script");
+    script.id = "meta-pixel-script";
+    script.async = true;
+    script.text = `!function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', ${process.env.NEXT_PUBLIC_PIXEL_ID});
+    fbq('track', 'PageView');`;
+    document.head.appendChild(script);
+    console.log("Pixel init");
 
     return () => {
-      // Cleanup: Remove the Facebook Pixel script
-      const fbScript = document.querySelector('script[src*="fbevents.js"]');
-      if (fbScript) {
-        fbScript.remove();
-        console.log("Pixel removed");
-      }
+      const addedScript = document.getElementById("meta-pixel-script");
+      if (addedScript) addedScript.remove();
     };
   }, []);
 
