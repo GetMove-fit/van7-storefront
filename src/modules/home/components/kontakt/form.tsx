48de18219d0e07744c3a35bf887b68adb7398005
Brendon Ignath <brendon.ignath@gmail.com>
2025-03-25 12:09:15 +0100

    Add recaptcha to form

--- src/modules/home/components/kontakt/form.tsx
+++ src/modules/home/components/kontakt/form.tsx
@@ -8,56 +8,68 @@ export default function KontaktFormular() {
   const t = useTranslations("form");
 
   return (
-    <form
-      action="https://formsubmit.co/info@van7.at"
-      method="POST"
-      className="flex max-w-lg flex-col gap-y-5"
-    >
-      <Input label={t("name")} name="Name" required autoComplete="name" />
-
-      <Input
-        label={t("phone")}
-        name="Telefonnummer"
-        type="tel"
-        required
-        autoComplete="tel"
-      />
-
-      <Input
-        label={t("email")}
-        name="E-Mail"
-        type="email"
-        required
-        autoComplete="email"
-      />
-
-      <Textarea name="Nachricht" required className="h-40" />
-
-      <label className="flex w-full flex-wrap items-center gap-x-2 text-lg">
-        <Checkbox required />
-        <span>
-          {t("agreement.prefix")}{" "}
-          <LocalizedClientLink href="/datenschutz">
-            {t("agreement.privacyPolicy")}
-          </LocalizedClientLink>
-          ,{" "}
-          <LocalizedClientLink href="/agb">
-            {t("agreement.terms")}
-          </LocalizedClientLink>{" "}
-          {t("agreement.and")}{" "}
-          <LocalizedClientLink href="/widerrufbelehrung">
-            {t("agreement.cancellationPolicy")}
-          </LocalizedClientLink>{" "}
-          {t("agreement.suffix")}
-        </span>
-      </label>
-
-      <button
-        type="submit"
-        className="mt-5 h-fit w-fit rounded bg-gradient-to-b from-brand-light to-brand-dark px-4 py-3 font-bold uppercase leading-none text-white transition-shadow hover:shadow-lg hover:shadow-brand-highlight/30 sm:text-lg"
+    <>
+      <script
+        src="https://www.google.com/recaptcha/api.js"
+        async
+        defer
+      ></script>
+      <form
+        action="https://formspree.io/f/xqaeobav"
+        method="POST"
+        className="flex max-w-lg flex-col gap-y-5"
       >
-        {t("submit")}
-      </button>
-    </form>
+        <Input label={t("name")} name="Name" required autoComplete="name" />
+
+        <Input
+          label={t("phone")}
+          name="Telefonnummer"
+          type="tel"
+          required
+          autoComplete="tel"
+        />
+
+        <Input
+          label={t("email")}
+          name="E-Mail"
+          type="email"
+          required
+          autoComplete="email"
+        />
+
+        <Textarea name="Nachricht" required className="h-40" />
+
+        <label className="flex w-full flex-wrap items-center gap-x-2 text-lg">
+          <Checkbox required />
+          <span>
+            {t("agreement.prefix")}{" "}
+            <LocalizedClientLink href="/datenschutz">
+              {t("agreement.privacyPolicy")}
+            </LocalizedClientLink>
+            ,{" "}
+            <LocalizedClientLink href="/agb">
+              {t("agreement.terms")}
+            </LocalizedClientLink>{" "}
+            {t("agreement.and")}{" "}
+            <LocalizedClientLink href="/widerrufbelehrung">
+              {t("agreement.cancellationPolicy")}
+            </LocalizedClientLink>{" "}
+            {t("agreement.suffix")}
+          </span>
+        </label>
+
+        <div
+          className="g-recaptcha"
+          data-sitekey="6LfcfP8qAAAAACPYI5xVPvAJ2rBRjZMIFFcWy1fG"
+        ></div>
+
+        <button
+          type="submit"
+          className="mt-5 h-fit w-fit rounded bg-gradient-to-b from-brand-light to-brand-dark px-4 py-3 font-bold uppercase leading-none text-white transition-shadow hover:shadow-lg hover:shadow-brand-highlight/30 sm:text-lg"
+        >
+          {t("submit")}
+        </button>
+      </form>
+    </>
   );
 }
