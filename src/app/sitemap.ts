09cfd8114ef5fc3e60c15c246df67dce6661482d
Brendon Ignath <brendon.ignath@gmail.com>
2025-04-01 17:07:01 +0200

    Add sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,36 @@
+import { listProducts } from "@lib/data/products";
+import type { MetadataRoute } from "next";
+
+const pages = ["/", "/hubbett-kaufen", "/datenschutz", "/agb", "/impressum"];
+const locales = ["de", "en"];
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const products = await listProducts({
+    countryCode: "at",
+    queryParams: {
+      fields: "handle",
+      collection_id: process.env.NEXT_PUBLIC_HUBBETT_COLLECTION_ID,
+    },
+  }).then(({ response }) => response.products);
+  pages.push(...products.map((product) => "/p/" + product.handle));
+
+  return pages.map((p) => {
+    const path = p === "/" ? "" : p;
+    return {
+      url: "https://van7.com/at/de" + path,
+      lastModified: new Date(),
+      alternates: {
+        languages: locales.reduce(
+          (acc, locale) => {
+            acc[locale] = `https://van7.com/at/${locale}${path}`;
+            return acc;
+          },
+          { "x-default": "https://van7.com/at/en" + path } as Record<
+            string,
+            string
+          >
+        ),
+      },
+    };
+  });
+}
